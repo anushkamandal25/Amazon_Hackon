c8faefcaca58412aab44889cc4cb61c95e8f61a4
anushkamandal25 <anushka.mandal03@gmail.com>
2023-10-22 03:51:57 +0530

    Updated register and login portal

--- hackon_chatbot/urls.py
+++ hackon_chatbot/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('chatbot.urls')),
+
 ]
